d445f4f67fdee8c24537e49686cba7f93d82d8df
shahrzad Tajvidi <shahrzad.tajvidi@fms-ea.com>
2024-07-23 12:55:42 +0200

    add docstring: explain the rules of game

--- batail_naval.py
+++ batail_naval.py
@@ -2,6 +2,14 @@
 # -*- coding: utf-8 -*-
 
 # Function to initialize the board
+"""
+Battleship game:
+Battleship (also known as Battleships) is a strategy type guessing game for two players.
+ It is played on ruled grids (paper or board) on which each player's fleet of warships are marked.
+ The locations of the fleets are concealed from the other player.
+ Players alternate turns calling "shots" at the other player's ships, and the objective of the game is to destroy the opposing player's fleet.
+"""
+
 def init_board(size):
     return [['-' for _ in range(size)] for _ in range(size)]
 
